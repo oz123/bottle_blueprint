7fd6b03b02cf058494151da72337452cf32e94ca
Oz N Tiram <oz.tiram@gmail.com>
2016-11-30 22:29:18 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest, os
 from webtest import TestApp
 
-from main import make_app
+try:
+    from main import make_app
+except ImportError:
+    print("If you see this, you didn't install the package before running the tests")
+    print("Please install the pacakge with 'pip install -e .'")
 
 app = make_app()
 
